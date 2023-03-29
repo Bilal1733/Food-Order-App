ecb8103791062ed42fdba84e708d6091dd4ff0f2
bkss1733 <89654469+bkss1733@users.noreply.github.com>
2023-03-29 10:27:33 +0530

    Using the Context.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import CartProvider from './store/CardProvider';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
