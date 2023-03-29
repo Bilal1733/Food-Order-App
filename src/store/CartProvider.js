d9df0ffcb24859f98ec3db0fc0b5c5b1d223501a
bkss1733 <89654469+bkss1733@users.noreply.github.com>
2023-03-29 13:56:18 +0530

    Working with Refs & Forward Refs.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,7 +7,7 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concate(action.item);
+    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
